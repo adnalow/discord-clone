832f86fdd299f4da8d45e2eb207f011cd5abe891
Adrian <reinieradrianluna@gmail.com>
2023-09-06 20:32:48 +0800

    dark/light theme done

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,3 +1,4 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { UserButton } from "@clerk/nextjs";
@@ -11,6 +12,7 @@ export default function Home() {
       <UserButton 
         afterSignOutUrl="/"
       />
+      <ModeToggle></ModeToggle>
     </div>
   );
 }
