cda1a9a5acb5b3f2e6ee63221f0dc9a0fdcf6f86
Adrian <reinieradrianluna@gmail.com>
2023-09-12 21:19:08 +0800

    Navigation sidebar OTP

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -3,6 +3,8 @@ import { redirect } from "next/navigation";
 import { currentProfile } from "@/lib/current-profile"
 import { db } from "@/lib/db";
 
+import { NavigationAction } from "./navigation-action";
+
 
 
 
@@ -27,7 +29,7 @@ export const NavigationSideBar = async () => {
         <div
             className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22]"
         >
-            Navigation Sidebar
+            <NavigationAction/>
         </div>
     )
 }
