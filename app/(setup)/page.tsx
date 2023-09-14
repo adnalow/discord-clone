c3ebd8ce2b2ed35b917afce9c1509cf77c51ad9f
Adrian <reinieradrianluna@gmail.com>
2023-09-14 20:33:24 +0800

    debugging

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -24,7 +24,7 @@ const SetupPage = async () => {
     }
 
 
-    return  <InitialModal />;
+    return  <InitialModal/>;
 }
  
 export default SetupPage;
--- app/api/servers/route.ts
+++ app/api/servers/route.ts
@@ -36,7 +36,7 @@ export async function POST(req: Request) {
     return NextResponse.json(server);
     } catch (error) {
         console.log("[SERVERS_POST", error);
-        return new NextResponse("Internal", { status: 500});
+        return new NextResponse("Internal Error", { status: 500});
     }
     
 }
