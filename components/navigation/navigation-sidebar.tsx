890310b7d97ba169be85e8493d80128d8f21bec2
Adrian <reinieradrianluna@gmail.com>
2023-09-15 21:41:14 +0800

    Navigation sidebar done

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -1,13 +1,17 @@
 import { redirect } from "next/navigation";
+import { UserButton } from "@clerk/nextjs";
 
 import { Separator } from "@/components/ui/separator";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { ModeToggle } from "@/components/mode-toggle";
 
 import { currentProfile } from "@/lib/current-profile"
 import { db } from "@/lib/db";
 
 import { NavigationAction } from "./navigation-action";
 import { NavigationItem } from "./navigation-item";
+
+
 
 
 
@@ -51,6 +55,18 @@ export const NavigationSideBar = async () => {
                 ))}
 
             </ScrollArea>
+            <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
+                <ModeToggle />
+                <UserButton 
+                    afterSignOutUrl="/"
+                    appearance={{
+                        elements: {
+                            avatarBox: "h-[48px] w-[48px]",
+                        }
+                    }}
+                />
+
+            </div>
         </div>
     )
 }
