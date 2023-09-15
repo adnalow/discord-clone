b9ae4d1d6e2afca224c9c5163045d250f272bf44
Adrian <reinieradrianluna@gmail.com>
2023-09-15 21:14:24 +0800

    Navigation sidebar OTP

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -1,11 +1,14 @@
 import { redirect } from "next/navigation";
 
 import { Separator } from "@/components/ui/separator";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 import { currentProfile } from "@/lib/current-profile"
 import { db } from "@/lib/db";
 
 import { NavigationAction } from "./navigation-action";
+import { NavigationItem } from "./navigation-item";
+
 
 
 
@@ -36,6 +39,18 @@ export const NavigationSideBar = async () => {
             <Separator
                 className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto"
             />
+            <ScrollArea className="flex-1 w-full"> 
+                {servers.map((server) => (
+                    <div key={server.id} className="mb-4">
+                        <NavigationItem 
+                            id={server.id}
+                            name={server.name}
+                            imageUrl={server.imageUrl}
+                        />
+                    </div>
+                ))}
+
+            </ScrollArea>
         </div>
     )
 }
