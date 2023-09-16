ddd75415ae488596b5ff384ef01ef1acdd50afd7
Adrian <reinieradrianluna@gmail.com>
2023-09-16 15:59:36 +0800

    Invitations OTP

--- components/modals/invite-modal.tsx
+++ components/modals/invite-modal.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Check, Copy, RefreshCw } from "lucide-react";
 
 import {
     Dialog,
@@ -8,12 +9,37 @@ import {
 } from "@/components/ui/dialog"
 
 import { useModal } from "@/hooks/use-modal-store";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useOrigin } from "@/hooks/use-origin";
+import { useState } from "react";
+import { set } from "zod";
 
 
 export const InviteModal = () => {
-    const { isOpen, onClose, type } =useModal();
+    const { isOpen, onClose, type, data } =useModal();
+    const origin = useOrigin();
 
     const isModalOpen = isOpen && type === "invite";
+    const { server } = data;
+
+    const [copied, setCopied] = useState(false);
+    const [isloading, setIsLoading] = useState(false);
+
+
+    const inviteUrl = `${origin}/invite/${server?.inviteCode}`;
+
+    const onCopy = () => {
+        navigator.clipboard.writeText(inviteUrl);
+        setCopied(true);
+
+        setTimeout(() => {
+            setCopied(false);
+        }, 1000);
+    }
+
+    
 
     return (
        <Dialog open={isModalOpen} onOpenChange={onClose}>
@@ -23,7 +49,30 @@ export const InviteModal = () => {
                         Invite Friends
                     </DialogTitle>
                 </DialogHeader>
-               Invite Modal!
+               <div className="p-6">
+                    <Label
+                        className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70"
+                    >
+                        Server Invite Link
+                    </Label>
+                    <div className="flex items-center mt-2 gap-x-2">
+                        <Input 
+                            className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
+                            value = {inviteUrl}
+                        />
+                        <Button onClick={onCopy} size ="icon">
+                            {copied ? <Check className="w-4 h-4" /> : <Copy className="w-4 h-4" />}
+                        </Button>
+                    </div>
+                    <Button
+                        variant="link"
+                        size="sm"
+                        className="text-xs text-zinc-500 mt-4"
+                    >
+                        Generate a new Link
+                        <RefreshCw className="w-4 h-4 ml-2" />
+                    </Button>
+               </div>
             </DialogContent>
        </Dialog>
     )
