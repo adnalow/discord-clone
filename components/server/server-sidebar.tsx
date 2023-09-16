616ebb9123900793e183e45ae9c7b1f59812bd33
Adrian <reinieradrianluna@gmail.com>
2023-09-16 14:59:46 +0800

    Server Sidebar done

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -4,6 +4,7 @@ import { redirect } from "next/navigation";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 
+import { ServerHeader } from "./server-header";
 
 interface ServerSideBarProps {
     serverId: string;
