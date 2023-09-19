2bd24d59133bc3bced818072514136d19e8c1468
Adrian <reinieradrianluna@gmail.com>
2023-09-19 10:18:00 +0800

    Invitations OTP

--- components/modals/invite-modal.tsx
+++ components/modals/invite-modal.tsx
@@ -1,5 +1,10 @@
 "use client";
+
+import axios from "axios";
+
 import { Check, Copy, RefreshCw } from "lucide-react";
+import { useOrigin } from "@/hooks/use-origin";
+import { useState } from "react";
 
 import {
     Dialog,
@@ -12,13 +17,10 @@ import { useModal } from "@/hooks/use-modal-store";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { useOrigin } from "@/hooks/use-origin";
-import { useState } from "react";
-import { set } from "zod";
 
 
 export const InviteModal = () => {
-    const { isOpen, onClose, type, data } =useModal();
+    const { onOpen ,isOpen, onClose, type, data } =useModal();
     const origin = useOrigin();
 
     const isModalOpen = isOpen && type === "invite";
@@ -39,6 +41,18 @@ export const InviteModal = () => {
         }, 1000);
     }
 
+    const onNew = async () => {
+        try {
+            setIsLoading(true);
+            const response = await axios.patch(`/api/servers/${server?.id}/invite-code`);    
+
+            onOpen("invite", { server: response.data });
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setIsLoading(false);
+        }
+    }
     
 
     return (
@@ -57,14 +71,17 @@ export const InviteModal = () => {
                     </Label>
                     <div className="flex items-center mt-2 gap-x-2">
                         <Input 
+                            disabled = {isloading}
                             className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
                             value = {inviteUrl}
                         />
-                        <Button onClick={onCopy} size ="icon">
+                        <Button disabled={isloading} onClick={onCopy} size ="icon">
                             {copied ? <Check className="w-4 h-4" /> : <Copy className="w-4 h-4" />}
                         </Button>
                     </div>
                     <Button
+                        onClick={onNew}
+                        disabled={isloading}
                         variant="link"
                         size="sm"
                         className="text-xs text-zinc-500 mt-4"
