aaf25aa0001978addcf9f1aee69777c32cca9b63
Adrian <reinieradrianluna@gmail.com>
2023-09-26 21:11:32 +0800

    Channel Creation OTP

--- components/modals/create-channel-modal.tsx
+++ components/modals/create-channel-modal.tsx
@@ -4,12 +4,12 @@ import axios from "axios";
 import * as z from "zod";
 import { zodResolver} from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { ChannelType } from "@prisma/client";
 
 
 import {
     Dialog,
     DialogContent,
-    DialogDescription,
     DialogFooter,
     DialogHeader,
     DialogTitle,
@@ -24,18 +24,28 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { FileUpload } from "@/components/file-upload";
 import { useRouter } from "next/navigation";
 import { useModal } from "@/hooks/use-modal-store";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue
+} from "@/components/ui/select"
+
 
 
 const formSchema = z.object({
     name: z.string().min(1, {
-        message: "Server Name is required."
-    }),
-    imageUrl: z.string().min(1, {
-        message: "Server image is required"
-    })
+        message: "Channel Name is required."
+    }).refine(
+        name => name != "general",
+        {
+            message: "Channel name cannot be 'general'"
+        }
+    ),
+    type: z.nativeEnum(ChannelType)
 });
 
 export const CreateChannelModal = () => {
@@ -49,7 +59,7 @@ export const CreateChannelModal = () => {
         resolver: zodResolver(formSchema),
         defaultValues: {
             name: "",
-            imageUrl: "",
+            type: ChannelType.TEXT,
         }
     });
 
@@ -77,49 +87,38 @@ export const CreateChannelModal = () => {
             <DialogContent className="bg-white text-black p-0 overflow-hidden">
                 <DialogHeader className="pt-8 px-6">
                     <DialogTitle className="text-2xl text-center font-bold">
-                        Customize your server
+                        Create Channel
                     </DialogTitle>
-                    <DialogDescription className="text-center text-zinc-500">
-                        Give your server a name and an image. You can always change it later
-                    </DialogDescription>
                 </DialogHeader>
                 <Form {...form}>
                     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
                         <div className="space-y-8 px-6">
-                            <div className="flex items-center justify-center text-center">
-                                <FormField 
-                                    control={form.control}
-                                    name="imageUrl"
-                                    render={({ field }) => (
-                                        <FormItem>
-                                            <FormControl>
-                                                <FileUpload
-                                                    endpoint="serverImage"
-                                                    value={field.value}
-                                                    onChange={field.onChange}
-                                                />
-                                            </FormControl>
-                                        </FormItem>
-                                    )}
-                                />
-                            </div>
-
                             <FormField control={form.control} name="name" render={({ field }) => (
                                 <FormItem>
                                     <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
-                                        Server Name 
+                                        Channel Name
                                     </FormLabel>
                                     <FormControl>
                                         <Input
                                             disabled = {isLoading}
                                             className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
-                                            placeholder="Enter Server name"
+                                            placeholder="Enter Channel name"
                                             {...field}
                                         />
                                     </FormControl>
                                     <FormMessage />
                                 </FormItem>
-                            )}    />
+                            )}    
+                          />
+                          <FormField
+                            control = {form.control}
+                            name="type"
+                            render={({ field }) => (
+                                <FormItem>
+
+                                </FormItem>
+                            )}
+                          />
                         </div>
                         <DialogFooter className="bg-gray-100 px-6 py-4">
                             <Button variant="primary" disabled={isLoading}>
