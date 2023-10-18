b77bd22701aae9c8f10c933a477a39d83ad365aa
Adrian <reinieradrianluna@gmail.com>
2023-10-18 21:50:38 +0800

    Chat Item component OTP

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -65,12 +65,39 @@ export const ChatItem = ({
     const [isEditing, setIsEditing] = useState(false);
     const [isDeleting, setIsDeleting] = useState(false);
 
+    useEffect(() => {
+        const handleKeyDown = (event: any) => {
+            if (event.key === "Escape" || event.keyCode === 27) {
+                setIsEditing(false);
+            }
+        };
+
+        window.addEventListener("keydown", handleKeyDown);
+
+        return () => window.removeEventListener("keydown", handleKeyDown);
+    }, []);
+
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
             content: content
         }
     });
+
+    const isLoading = form.formState.isSubmitting;
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try {
+            const url = qs.stringifyUrl({
+                url: `${socketUrl}/${id}`,
+                query: socketQuery,
+            });
+
+            await axios.patch(url, values);
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
     useEffect(() => {
         form.reset({
@@ -154,7 +181,37 @@ export const ChatItem = ({
                     {!fileUrl && isEditing && (
                         <Form {...form}>
                             <form>
-                                
+                                <form 
+                                    className="flex items-center w-full gap-x-2 pt-2"
+                                    onSubmit={form.handleSubmit(onSubmit)}
+                                >
+                                    <FormField 
+                                        control={form.control}
+                                        name="content"
+                                        render={({ field }) => (
+                                            <FormItem
+                                                className="flex-1"
+                                            >
+                                                <FormControl>
+                                                    <div className="relative w-full">
+                                                        <Input 
+                                                             disabled={isLoading}
+                                                             className="p-2 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200"
+                                                             placeholder="Edited Message"
+                                                             {...field}
+                                                        />
+                                                    </div>
+                                                </FormControl>
+                                            </FormItem>
+                                        )}
+                                    />
+                                    <Button disabled={isLoading} size="sm" variant="primary">
+                                        Save
+                                    </Button>
+                                </form>
+                                <span className="text-[10px] mt-1 text-zinc-400">
+                                    Press escape to cancel, enter to save
+                                </span>
                             </form>
                         </Form>
                     )}
