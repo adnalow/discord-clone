367e761b22206539c45d577bc1347782be32a947
Adrian <reinieradrianluna@gmail.com>
2023-10-20 22:37:07 +0800

    Chat Item component debugging OTP

--- pages/api/socket/messages/[messageId].ts
+++ pages/api/socket/messages/[messageId].ts
@@ -1,6 +1,7 @@
 import { currentProfilePages } from "@/lib/current-profile-pages";
 import { NextApiResponseServerIo } from "@/types";
 import { NextApiRequest } from "next";
+import {db } from "@/lib/db";
 
 export default async function handler(
     req: NextApiRequest,
@@ -22,6 +23,14 @@ export default async function handler(
         if (!serverId) {
             return res.status(400).json({error: "Server ID missing" });
         }
+
+        if (!channelId) {
+            return res.status(400).json({error: "Channel ID missing" });
+        }
+
+        const server = await db.
+
+
     } catch (error) {
         console.log("[MESSAGE_ID]", error);
         return res.status(500).json({error: "Internal Error" });
