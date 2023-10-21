82b189948c631de68a1f9c99ddc60b9c8ff3c748
Adrian <reinieradrianluna@gmail.com>
2023-10-21 15:26:26 +0800

    Video Calls done

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -4,6 +4,8 @@ import { MobileToggle } from "@/components/mobile-toggle";
 import { UserAvatar } from "@/components/user-avatar";
 import { SocketIndicator } from "@/components/socket-indicator";
 
+import { ChatVideoButton } from "./chat-video-button";
+
 interface ChatHeaderProps {
     serverId: string;
     name: string;
@@ -33,10 +35,9 @@ export const ChatHeader = ({
                 {name}
             </p>
             <div className="ml-auto flex items-center">
-                <Github className=" text-green-500"/>
-                <p className="font-semibold text-sm hover:underline cursor-pointer">
-                    https://github.com/adnalow
-                </p>
+                {type === "conversation" && (
+                    <ChatVideoButton />
+                )}
                 <SocketIndicator />
             </div>
         </div>
